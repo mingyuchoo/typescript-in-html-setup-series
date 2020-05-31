afce49ca099f08af79a81c244fe9efa6558357c6
mingyuchoo <mingyuchoo@gmail.com>
2020-05-31 10:14:29 +0900

    modified

--- 00-initial-WIP/src/ts/index.ts
+++ 00-initial-WIP/src/ts/index.ts
@@ -2,3 +2,14 @@
 export const sum = (a: number, b: number) => {
   return a + b;
 };
+
+/* msg */
+export const msg = () => {
+  alert('Hello, JavaScript!');
+};
+
+/* isValidPwdRegex */
+export const isValidPwdRegex = (pwd: string) => {
+  const regex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[-~!@$%^&*=_+,.;':"\[\]{}\\]).{8,20}$/;
+  return regex.test(pwd);
+};
--- 00-initial-WIP/webpack.config.babel.js
+++ 00-initial-WIP/webpack.config.babel.js
@@ -14,17 +14,18 @@ export default {
   module: {
     rules: [
       {
-        test: /\.ts$/,
+        test: /\.(ts|js)$/,
         include: path.join(__dirname),
         exclude: /(node_modules)|(public)/,
         use: {
           loader: 'babel-loader',
-          options: {
-            presets: [
-              '@babel/preset-env',
-              '@babel/preset-typescript'
-            ],
-          },
+          // babel.config.json 의 내용을 먼저 읽어 들이므로 아래 는 주석 처리함
+          // options: {
+          //   presets: [
+          //     '@babel/preset-env',
+          //     '@babel/preset-typescript'
+          //   ],
+          // },
         },
       }
     ],
@@ -33,6 +34,6 @@ export default {
   optimization: {},
   resolve: {
     modules: ['node_modules'],
-    extensions: ['.ts', '.tsx', '.js'],
+    extensions: ['.ts', '.js'],
   },
 };
